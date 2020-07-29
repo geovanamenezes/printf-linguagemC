705ba657762b7e22431a8714c6d919b8289f5334
Geovana Menezes <56615628+geovanamenezes@users.noreply.github.com>
2020-07-28 22:55:11 -0300

    Add files via upload

---
+++ Exercício3b-Lista1.c
@@ -0,0 +1,31 @@
+
+/*
+ Use como exemplo o código abaixo 
+ 
+	#include <stdio.h> 
+ 
+	int main() {  
+	 	int num1, num2, soma; 
+		num1 = 10;   num2 = 5; 
+ 		soma = num1 + num2; 
+ 		printf(“soma = %d\n”, soma); 
+ 
+  		return 0; 
+		}
+		
+	Faça outros três códigos, um para:
+	a) subtração; b) multiplicação; c) divisão.  
+ 
+	Utilize variáveis float para o a letra c (divisão) 
+	(falaremos mais sobre conversão de tipo de variáveis nas próximas aulas). 
+*/
+#include <stdio.h>
+int main(){
+	int num1, num2, multiplicacao;
+	num1 = 10;
+	num2 = 5;
+	multiplicacao = num1*num2;
+	printf("multiplicacao: = %d", multiplicacao);
+	return 0;
+}
+
